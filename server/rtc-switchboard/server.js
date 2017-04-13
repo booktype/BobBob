1e80f760db9046a3a04a9ac4e362e89efeabd39f
vkefallinos <vkefallinos@gmail.com>
2017-04-13 17:55:26 +0300

    change default port

--- server/rtc-switchboard/server.js
+++ server/rtc-switchboard/server.js
@@ -1,6 +1,6 @@
 var server = require('http').createServer();
 var switchboard = require('./')(server, { servelib: true });
-var port = parseInt(process.env.NODE_PORT || process.env.PORT || process.argv[2], 10) || 3000;
+var port = parseInt(process.env.NODE_PORT || process.env.PORT || process.argv[2], 10) || 8997;
 var host = process.env.NODE_HOST || process.env.HOST || '0.0.0.0';
 var replify = require('replify');
 
