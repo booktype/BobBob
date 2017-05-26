f4ec15a55a0e475aaa5a3d36648f6339ac1f2376
vkefallinos <vkefallinos@gmail.com>
2017-05-26 20:48:14 +0300

    add font color and font background color menu buttons

---
+++ src/components/EditorBar/Home/HighlightColorPicker.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import ColorPicker from './ColorPicker/Color'
+import IconButton from 'material-ui/IconButton';
+import FormatColorFill from '../../../icons/formatColorFill'
+import Popover from 'material-ui/Popover';
+import {RichUtils} from 'draft-js';
+
+
+export default class HighlightColorPicker extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+      color: null
+    };
+  }
+  componentWillReceiveProps(nextProps){
+    const currentColorStyle = nextProps.controller.getStyleType("backgroundColor")
+    if(currentColorStyle){
+      const color = currentColorStyle.split("__")[1]
+      if(color!==this.state.color)
+        this.setState({
+          color
+        })
+    }else{
+      if(this.state.color!=="#000000")
+        this.setState({
+          color: null
+        })
+    }
+
+  }
+  handleTouchTap = (event) => {
+    // This prevents ghost click.
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget,
+    });
+  };
+  handleColorChange = (color) => {
+    if(this.state.color){
+      this.props.onChange(
+        RichUtils.toggleInlineStyle(
+          RichUtils.toggleInlineStyle(
+            this.props.controller.editorState,
+            `backgroundColor__${this.state.color}`,
+          ),
+          `backgroundColor__${color.hex}`,
+        )
+      );
+    }else{
+      this.props.onChange(
+        RichUtils.toggleInlineStyle(
+          this.props.controller.editorState,
+          `backgroundColor__${color.hex}`,
+        )
+      );
+    }
+    this.setState({color: color.hex})
+
+  }
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  render() {
+    return (
+      <div style={{display: "inline-block"}}>
+        <IconButton
+          onTouchTap={this.handleTouchTap}
+          tooltip="Highlight Color"
+          >
+            <FormatColorFill color={this.state.color}/>
+          </IconButton>
+          <Popover
+            open={this.state.open}
+            anchorEl={this.state.anchorEl}
+            anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+            targetOrigin={{horizontal: 'left', vertical: 'top'}}
+            onRequestClose={this.handleRequestClose}
+            >
+              <ColorPicker onChange={this.handleColorChange}/>
+          </Popover>
+      </div>
+    )
+  }
+}
