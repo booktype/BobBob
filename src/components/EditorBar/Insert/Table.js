34930d81a8f2a5997d06c9c490ab17134e5c9fbd
vkefallinos <vkefallinos@gmail.com>
2017-06-02 15:55:55 +0300

    add table menu controller

---
+++ src/components/EditorBar/Insert/Table.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import TablePicker from './TablePicker';
+import IconButton from 'material-ui/IconButton';
+import Popover from 'material-ui/Popover';
+import TableIcon from '../../../icons/table';
+import Badge from 'material-ui/Badge';
+export default class TableButton extends React.PureComponent {
+  constructor(props){
+    super(props)
+    this.state = {
+      open: false,
+      sizeX: 0,
+      sizeY: 0
+    };
+  }
+
+  handleTouchTap = (event) => {
+    // This prevents ghost click.
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget,
+    });
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+  handleSizeChange = (size) => {
+    console.log(size)
+    this.setState({...size})
+  }
+  handleChosenSize = () => {
+    this.handleRequestClose()
+    this.props.controller.insertElementAfter("table")
+    .appendChild("tbody")
+    for(let x = 0; x<this.state.sizeX;x++){
+      this.props.controller.appendChild("tr")
+      .appendChildren("td", this.state.sizeY).queryParent("tbody")
+    }
+    this.props.onChange(this.props.controller.editorState)
+    this.setState({sizeX:0, sizeY: 0})
+  }
+  render(){
+    return (
+      <div>
+        <Badge
+          badgeContent={`${this.state.sizeX}, ${this.state.sizeY}`}
+          secondary={true}
+          badgeStyle={{top: 10, left: 10, height: 40, width: 40}}
+        >
+
+        <IconButton
+          onTouchTap={this.handleTouchTap}
+          style={{height: "48px"}}
+          iconStyle={{width: "48px", height: "48px"}}
+          label={"Insert table"}
+        >
+          <TableIcon />
+        </IconButton>
+      </Badge>
+        <Popover
+          open={this.state.open}
+          anchorEl={this.state.anchorEl}
+          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+          targetOrigin={{horizontal: 'left', vertical: 'top'}}
+          onRequestClose={this.handleRequestClose}
+        >
+          <TablePicker onChange={this.handleSizeChange} onSelect={this.handleChosenSize} />
+        </Popover>
+      </div>
+    );
+  }
+}
---
+++ src/components/EditorBar/Insert/TablePicker/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import DimensionHighlighted from "./dimension-highlighted.png";
+import DimensionUnhighlighted from "./dimension-unhighlighted.png";
+
+const unhighlighted = {
+  background: `url(${DimensionUnhighlighted})`,
+  maxWidth: "20em",
+  maxHeight: "20em"
+};
+const highlighted = {
+  background: `url(${DimensionHighlighted})`,
+  maxWidth: "20em",
+  maxHeight: "20em"
+};
+const wrapper = {
+  width: "20em",
+  height: "20em",
+  overflowY: "hidden"
+}
+
+export default class TableSizePicker extends React.PureComponent {
+  constructor(props){
+    super(props)
+    this.state= {
+      x: 0,
+      y: 0
+    }
+  }
+  resize = (e)=>{
+    let x,y;
+    console.log(e.pageX, e.pageY)
+    console.log(e.clientX, e.clientY)
+    const bounds = e.target.getBoundingClientRect()
+    x = e.pageX-bounds.left
+    y = e.pageY-bounds.top
+    const sizeX = 1-x/18%1 + x/18
+    const sizeY = 1-y/18%1 + y/18
+    x = 1-x/18%1 + x/16
+    y = 1-y/18%1 + y/16
+    this.setState({x,y,  sizeX, sizeY})
+    this.props.onChange({sizeX, sizeY})
+  }
+
+  render(){
+    return (
+        <div onMouseMove={this.resize} onClick={this.props.onSelect}style={wrapper}>
+    <div style={{width: "100%", height:"100%"}}>
+    <div style={{...unhighlighted,
+  position: "absolute",
+    top:0,left:0,width:`${this.state.x+1}em`, height:`${this.state.y+1}em`
+                }}></div>
+      <div style={{...highlighted,
+           position: "absolute",
+    top:0,left:0,
+          width:`${this.state.x}em`, height:`${this.state.y}em`}}></div>
+
+      </div>
+        {this.state.sizeX},{this.state.sizeY}
+  </div>
+    )
+  }
+}
