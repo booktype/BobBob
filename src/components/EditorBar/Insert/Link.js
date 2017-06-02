4012692022d5f8528c0eb2fb58bf13915bbc5da6
vkefallinos <vkefallinos@gmail.com>
2017-06-02 15:55:25 +0300

    add link menu controller

---
+++ src/components/EditorBar/Insert/Link.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import IconButton from 'material-ui/IconButton';
+import Popover from 'material-ui/Popover';
+import LinkIcon from '../../../icons/link';
+import LinkOffIcon from '../../../icons/linkOff';
+import TextField from 'material-ui/TextField';
+import {RichUtils} from 'draft-js'
+export default class LinkButton extends React.PureComponent {
+  constructor(props){
+    super(props)
+    this.state = {
+      open: false,
+      url: ""
+    };
+  }
+
+  handleTouchTap = (event) => {
+    // This prevents ghost click.
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget,
+    });
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+  handleUrlChange = (e, url) => {
+    this.setState({
+      url
+    })
+  }
+  componentWillReceiveProps(nextProps){
+    const data = nextProps.controller.getCurrentMetaData("LINK")
+    if(data && data.attributes && data.attributes.href){
+      this.setState({url: data.attributes.href})
+    }else if(this.state.url){
+      this.setState({url: ""})
+    }
+  }
+  removeLink = () => {
+    this.props.onChange(
+      RichUtils.toggleInlineStyleWithMeta(
+        this.props.controller.editorState,
+        "LINK",
+        {
+        }
+      )
+    );
+  }
+  handleUrlSubmit = () => {
+    this.handleRequestClose()
+    let href;
+    if(!this.state.url.startsWith("http://") && !this.state.url.startsWith("https://")){
+      href = `http://${this.state.url}`
+    }else{
+      href = this.state.url
+    }
+    if(this.props.controller.currentInlineStyle.has("LINK")){
+      this.props.onChange(
+        this.props.controller.replaceStyleMetaData(
+          "LINK",
+          {
+            attributes: {
+              href
+            }
+          }
+        ).editorState
+      )
+    }else{
+      this.props.onChange(
+        RichUtils.toggleInlineStyleWithMeta(
+          this.props.controller.editorState,
+          "LINK",
+          {
+            attributes: {
+              href
+            }
+          }
+        )
+      );
+    }
+  }
+  render(){
+    return (
+      <div>
+        <IconButton
+          onTouchTap={this.handleTouchTap}
+          style={{height: "48px"}}
+          iconStyle={{width: "48px", height: "48px"}}
+          label={"Insert Link"}
+        >
+          <LinkIcon color={this.state.url?"orange":null}/>
+        </IconButton>
+        <IconButton
+          onTouchTap={this.removeLink}
+          disabled={!this.state.url}
+          style={{height: "24px"}}
+          iconStyle={{width: "24px", height: "24px"}}
+          label={"Remove Link"}
+        >
+          <LinkOffIcon color={this.state.url?"orange":null}/>
+        </IconButton>
+        <Popover
+          open={this.state.open}
+          anchorEl={this.state.anchorEl}
+          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+          targetOrigin={{horizontal: 'left', vertical: 'top'}}
+          onRequestClose={this.handleRequestClose}
+          style={{overflowY: "hidden"}}
+        >
+          <div>
+            <TextField
+              hintText="Url"
+              floatingLabelText="Type Link and hit Enter"
+              type="url"
+              value={this.state.url}
+              onChange={this.handleUrlChange}
+              onKeyPress={(e)=>{e.key==="Enter"?this.handleUrlSubmit():null}}
+            />
+          </div>
+        </Popover>
+      </div>
+    );
+  }
+}
