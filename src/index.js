21a429f786d5a4b3da895fefe660fd76005bf1ba
olegpshenichniy <olegpshenichniy@gmail.com>
2017-10-12 16:49:34 +0300

    Get documentID public method

--- src/index.js
+++ src/index.js
@@ -34,7 +34,11 @@ window.EditorController = class {
 
   destroyEditor() {
     this._api.documentID = null;
-    ReactDOM.unmountComponentAtNode(this._container);
+    return ReactDOM.unmountComponentAtNode(this._container);
+  }
+
+  getDocumentID () {
+    return this._api.documentID
   }
 };
 
