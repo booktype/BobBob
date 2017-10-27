90c77e1139e06594f91d1eaf238fc8e36274a326
vkefallinos <vkefallinos@gmail.com>
2017-10-27 22:40:39 +0300

    replace material components with tachyons css custom components

--- src/components/ControllerContainer.js
+++ src/components/ControllerContainer.js
@@ -1,11 +1,4 @@
 import React from 'react';
-import {Tabs, Tab} from 'material-ui/Tabs';
-import {
-  Toolbar,
-  ToolbarGroup,
-  ToolbarSeparator,
-  ToolbarTitle
-} from 'material-ui/Toolbar';
 import EntityStyleControls from './EntityStyleControls';
 import FontColorPicker from './EditorBar/Home/FontColorPicker'
 import HighlightColorPicker from './EditorBar/Home/HighlightColorPicker'
@@ -25,59 +18,12 @@ import PictureForm from './EditorBar/Insert/Picture'
 import CommentButton from './EditorBar/Insert/Comment'
 import TableTab from './EditorBar/Table/Layout'
 
+import ETabs from './Tabs/Tabs';
+import ETab from './Tabs/Tab';
+
+
 
 const styles = {
-  tabButton: {
-    color: 'rgb(50, 60, 69)',
-    height: 24
-  },
-  controller: {
-    backgroundColor: "white",
-    marginLeft: 0,
-    width: "100%",
-    zIndex: 1000
-  },
-  "ribbon_toolbar": {
-    "position": "flex",
-    "float": "left"
-  },
-  "ribbon_group": {
-    "position": "relative",
-    "height": "92px",
-    "float": "left",
-    "borderStyle": "solid",
-    "borderWidth": "1px",
-    "borderColor": "transparent",
-    "padding": "20px",
-    "MozBoxSizing": "content-box",
-    "WebkitBoxSizing": "content-box",
-    "boxSizing": "content-box"
-  },
-  ribbon_row: {
-    "display": "flex",
-    "WebkitFlexDirection": "row",
-    "flexDirection": "row"
-  },
-  "ribbon_group_title": {
-    "position": "absolute",
-    // "left": "50",
-    "bottom": "0",
-    // "textAlign": "center",
-    "width": "130%",
-    "lineHeight": "15px",
-  },
-  "ribbon_group__l_btn": {
-    "float": "left"
-  },
-  "ribbon_group_sep": {
-    "float": "left",
-    "width": "0",
-    "height": "102px",
-    "fontSize": "0",
-    "margin": "0 2px",
-    "borderLeft": "1px solid #ccc",
-    "borderRight": "1px solid #fff"
-  }
 };
 
 
@@ -89,102 +35,42 @@ class ControllerContainer extends React.Component {
   onChange = (editorState) => {
     this.props.onChange(editorState)
   }
+  componentWillReceiveProps(nextProps){
+
+  }
 
   render() {
     return (
-      <div style={styles.controller}>
-        <Tabs tabItemContainerStyle={{height: 24}}>
-          <Tab buttonStyle={styles.tabButton} label="Home">
-            <Toolbar style={{height: 90}}>
-              <ToolbarGroup firstChild={true}>
-                <div style={styles.ribbon_row}>
-                  <FontPicker controller={this.props.controller} onChange={this.onChange}/>
-                  <FontSizePicker controller={this.props.controller} onChange={this.onChange}/>
-                </div>
-                <div>
-                  <FontColorPicker controller={this.props.controller} onChange={this.onChange}/>
-                  <HighlightColorPicker controller={this.props.controller} onChange={this.onChange}/>
-                </div>
-
-                <div style={styles.ribbon_toolbar}>
-                  <FontStyle controller={this.props.controller} onChange={this.onChange}/>
-                </div>
-                <ToolbarTitle text="Font" style={styles.ribbon_group_title}/>
-              </ToolbarGroup>
-
-              <ToolbarGroup firstChild={true}>
-                <div style={{display: "flex"}}>
-                  <ParagraphAlign controller={this.props.controller} onChange={this.onChange}/>
-                  <Indent controller={this.props.controller} onChange={this.onChange}/>
-                </div>
-                <div style={{display: "inline"}}>
-                  <UnorderedList controller={this.props.controller} onChange={this.onChange}/>
-                  <OrderedList controller={this.props.controller} onChange={this.onChange}/>
-                </div>
-                <div style={{display: "flex"}}>
-                  <LineSpacing controller={this.props.controller} onChange={this.onChange}/>
-                  <TextDirection controller={this.props.controller} onChange={this.onChange}/>
-                </div>
-                <ToolbarTitle text="Paragraph" style={styles.ribbon_group_title}/>
-
-              </ToolbarGroup>
-              <ToolbarGroup firstChild={true} style={styles.ribbon_group}>
-                <ParagraphStyles controller={this.props.controller} onChange={this.onChange}/>
-              </ToolbarGroup>
-            </Toolbar>
+      <div style={{zoom: 0.73}}>
+        <Tabs >
+          <Tab label={"Home"}>
+            <FontPicker fontFamily={this.props.inlineStyles.fontFamily} controller={this.props.controller} onChange={this.onChange}/>
+            <FontSizePicker fontSize={this.props.inlineStyles.fontSize} controller={this.props.controller} onChange={this.onChange}/>
+            <FontColorPicker color={this.props.inlineStyles.color} controller={this.props.controller} onChange={this.onChange}/>
+            <HighlightColorPicker backgroundColor={this.props.inlineStyles.backgroundColor} controller={this.props.controller} onChange={this.onChange}/>
+            <FontStyle styles={this.props.inlineStyles} controller={this.props.controller} onChange={this.onChange}/>
+            <ParagraphAlign textAlign={this.props.blockStyle.style.textAlign} controller={this.props.controller} onChange={this.onChange}/>
+            <Indent marginLeft={this.props.blockStyle.style.marginLeft} controller={this.props.controller} onChange={this.onChange}/>
+            <OrderedList ol={this.props.blockTree['ol']} controller={this.props.controller} onChange={this.onChange}/>
+            <UnorderedList ul={this.props.blockTree['ul']} controller={this.props.controller} onChange={this.onChange}/>
+            <LineSpacing lineHeight={this.props.blockStyle.style.lineHeight} controller={this.props.controller} onChange={this.onChange}/>
+            <TextDirection direction={this.props.blockStyle.style.direction} controller={this.props.controller} onChange={this.onChange}/>
+            <ParagraphStyles style={this.props.blockStyle.type} controller={this.props.controller} onChange={this.onChange}/>
           </Tab>
-          <Tab label="Insert" buttonStyle={styles.tabButton}>
-            <Toolbar style={{height: 90}}>
-              <ToolbarGroup firstChild={true}>
-                <div>
-                  <TablePicker controller={this.props.controller} onChange={this.onChange}/>
-
-                </div>
-
-                <ToolbarTitle text="Table" style={styles.ribbon_group_title}/>
-              </ToolbarGroup>
-              <ToolbarGroup>
-                <div>
-                  <LinkForm controller={this.props.controller} onChange={this.onChange}/>
-                </div>
-
-                <ToolbarTitle text="Link" style={styles.ribbon_group_title}/>
-              </ToolbarGroup>
-              <ToolbarGroup style={{width: 100}}>
-                <div>
-                  <PictureForm controller={this.props.controller} onChange={this.onChange}/>
-                </div>
-
-                <ToolbarTitle text="Picture" style={styles.ribbon_group_title}/>
-              </ToolbarGroup>
-              <ToolbarGroup style={{width: 100}}>
-                <div>
-                  <CommentButton controller={this.props.controller} onChange={this.onChange}/>
-                </div>
-
-                <ToolbarTitle text="Comment" style={styles.ribbon_group_title}/>
-              </ToolbarGroup>
-              <ToolbarGroup style={{width: 500}}>
-              </ToolbarGroup>
-
-
-            </Toolbar>
-          </Tab>
-          <Tab label="Review" buttonStyle={styles.tabButton}>
-            <EntityStyleControls controller={this.props.controller} onChange={this.onChange}
-                                 setReadOnly={this.props.setReadOnly}
-                                 hoverTarget={this.props.hoverTarget}
-                                 clickTarget={this.props.clickTarget}/>
-
+          <Tab label={"Insert"}>
+            <TablePicker controller={this.props.controller} onChange={this.onChange}/>
+            <LinkForm controller={this.props.controller} onChange={this.onChange}/>
+            <PictureForm controller={this.props.controller} onChange={this.onChange}/>
+            <CommentButton controller={this.props.controller} onChange={this.onChange}/>
           </Tab>
           {this.props.controller.location.find(block => {
             return block.getType() === "table"
           }
           ) ?
-            <Tab label="Table Layout" buttonStyle={styles.tabButton}>
+            <Tab label="Table Layout" >
               <TableTab controller={this.props.controller} onChange={this.onChange}/>
             </Tab>
-            : null}
+          : null}
         </Tabs>
       </div>
     )
---
+++ src/components/Tabs/Tab.js
@@ -0,0 +1,9 @@
+import React, {PureComponent} from 'react';
+
+export default (props) => {
+  return (
+    <div className="ph3 f4">
+      {props.children}
+    </div>
+  )
+}
