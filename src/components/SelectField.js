900902f381c9a1816beac84a54df197e6bc2af82
vkefallinos <vkefallinos@gmail.com>
2017-11-08 00:14:26 +0200

    create SelectField and Option component to replace material-ui relevant components

---
+++ src/components/SelectField.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function SelectField(props){
+  return (
+    <div style={{display: 'inline-block'}}>
+      <label style={{display: 'block'}}>
+         {props.label}
+      </label>
+      <select
+        className={props.className}
+        style={props.style}
+        value={props.value}
+        onChange={(e)=>{e.preventDefault();props.onChange(e.target.value)}}
+      >
+        {props.children}
+      </select>
+    </div>
+  )
+}
+
+export function Option(props) {
+  return (
+    <option
+      style={props.style}
+      className={props.className}
+      value={props.value}
+      >
+      {props.primaryText}
+    </option>
+  )
+}
