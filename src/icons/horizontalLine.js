23b7853650301168058b646c13ef15a5c1e42c64
olegpshenichniy <olegpshenichniy@gmail.com>
2017-11-21 14:05:10 +0100

    BTBOB-22 Horizontal line

---
+++ src/icons/horizontalLine.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SvgIcon from '../components/SvgIcon';
+
+
+export default (props) => (
+  <SvgIcon {...props}>
+    <path d="M19,13H5V11H19V13Z"/>
+  </SvgIcon>
+);
