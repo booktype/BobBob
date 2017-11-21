80c19a43bf99e6b96ef421e677eee2920148c8e0
olegpshenichniy <olegpshenichniy@gmail.com>
2017-11-21 16:11:33 +0100

    BTBOB-35 Improve IconButton component

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -10,8 +10,17 @@ export default class IconButton extends React.PureComponent {
 
   render() {
     const {tooltip, style, iconStyle} = this.props;
+    let buttonClasses = [
+      'icon-button', 'qs', 'f6', 'link', 'dim', 'br1', 'ba', 'ph1', 'pv1', 'mb1', 'dib',
+      this.props.disabled ? 'bg-moon-gray' : 'bg-white',
+      'black'
+    ];
     return (
-      <button style={style} onClick={this.props.onTouchTap} className={'qs f6 link dim br1 ba  ph1 pv1 mb1 dib bg-white black'}>
+      <button
+        style={style}
+        disabled={this.props.disabled}
+        onClick={this.props.onTouchTap}
+        className={buttonClasses.join(' ')}>
         <div className={'popover'}>
           <span>
             {tooltip}
